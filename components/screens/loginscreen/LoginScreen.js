3837650c6dcc4226a8011628121d12e3bfb63c66
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-04 19:41:33 +1200

    Started feature

---
+++ components/screens/loginscreen/LoginScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, KeyboardAvoidingView } from "react-native";
+import React from "react";
+
+const LoginScreen = () => {
+  return (
+    <KeyboardAvoidingView>
+      <Text>LoginScreen</Text>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
