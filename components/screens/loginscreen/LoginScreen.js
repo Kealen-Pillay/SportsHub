5f5cab7a6df1bbce884d4744b110de0eba716d9c
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-04 21:06:24 +1200

    Completed UI of Login Screen

--- components/screens/loginscreen/LoginScreen.js
+++ components/screens/loginscreen/LoginScreen.js
@@ -18,14 +18,22 @@ const LoginScreen = () => {
         source={require("../../../images/SportsHubLogo.png")}
       />
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Email"/>
-        <TextInput style={styles.textInput} placeholder="Password"/>
+        <TextInput style={styles.textInput} placeholder="Email" />
+        <TextInput style={styles.textInput} placeholder="Password" />
       </View>
-<View style={styles.buttonContainer}>
-<TouchableOpacity>
-  <Text>Login</Text>
-</TouchableOpacity>
-</View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.loginButton}>
+          <Text style={styles.buttonText}>
+            Login
+          </Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>Don't have an account?</Text>
+        <TouchableOpacity style={styles.registerButton}>
+          <Text style={styles.buttonText}>
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
     </KeyboardAvoidingView>
   );
 };
@@ -36,7 +44,8 @@ const styles = StyleSheet.create({
   image: {
     width: 200,
     height: 200,
-    marginBottom: 10,
+    marginBottom: 30,
+    marginTop: 70,
   },
   container: {
     flex: 1,
@@ -45,14 +54,48 @@ const styles = StyleSheet.create({
     backgroundColor: colours.backgroundDark,
     width: "100%",
   },
-  inputContainer:{
-    width: '80%',
+  inputContainer: {
+    width: "80%",
+    marginTop: 20,
   },
   textInput: {
-    marginTop: 10,
-    height: 40,
+    marginTop: 20,
+    height: 50,
     backgroundColor: colours.text,
     borderRadius: 5,
     paddingLeft: 10,
+  },
+  buttonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "60%",
+    marginTop: 50,
+    marginBottom: 100,
+  },
+  loginButton: {
+    backgroundColor: colours.purple,
+    height: 50,
+    width: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: colours.text,
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  registerButton: {
+    backgroundColor: colours.pink,
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    width: "70%",
+    borderRadius: 5,
+  },
+  text: {
+    color: colours.text,
+    margin: 20,
+    fontWeight: "bold",
   }
 });
