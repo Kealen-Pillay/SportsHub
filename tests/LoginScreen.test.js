2f5ac7c5d7f5016a2c650ee032843d85e6bc4d76
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-05 18:33:11 +1200

    Configured jest fo react native

--- tests/LoginScreen.test.js
+++ tests/LoginScreen.test.js
@@ -1,10 +1,12 @@
 import LoginScreen from "../components/screens/loginscreen/LoginScreen"
 
 //Tests that the Login Screen Renders
-it("Given I want to use the app, when I first open the app, then the login screen renders", () => {
+test("Given I want to use the app, when I first open the app, then the login screen renders", () => {
     const {getByPlaceholderText} = render(<LoginScreen/>);
     getByPlaceholderText("Email:");
 });
+
+
 
 //Tests that when a user types in the email text input field, the input is displayed
 
