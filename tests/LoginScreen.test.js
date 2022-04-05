1c1964198cb4a8152cdccb4591d95ac569b39094
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-05 13:24:20 +1200

    Added component to test file

---
+++ tests/LoginScreen.test.js
@@ -0,0 +1,8 @@
+import LoginScreen from "../components/screens/LoginScreen/LoginScreen"
+
+test('should see email appear in input field', () => {
+
+    const loginScreen = create(<LoginScreen/>); 
+    
+
+    })
