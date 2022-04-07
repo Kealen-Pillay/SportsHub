4f80b1f3bb10d56cff1a7755b9a65ab6a5e2dbed
Cam <greayham@gmail.com>
2022-04-07 22:49:49 +1200

    Created basic UI

---
+++ components/screens/Register/RegisterScreen.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
+
+
+const RegisterScreen = () => {
+    return (
+        <KeyboardAvoidingView>
+            <View>
+                <TextInput
+                    placehold="Username"
+                />
+
+                <TextInput
+                    placeholder="Email"
+                />
+
+                <TextInput
+                    placeholder="Password"
+                />
+
+                <TextInput
+                    palceholder="Confirm Password"
+                />
+
+            </View>
+
+            <View>
+                <TouchableOpacity>
+                    <Text>Create Account</Text>
+                </TouchableOpacity>
+            </View>
+
+        </KeyboardAvoidingView>
+    );
+}
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'grey',
+    },
+
+    logo: {
+
+        width: 100,
+        height: 100,
+        position: "absolute",
+        top: 70,
+    }
+})
