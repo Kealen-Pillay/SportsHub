0d05dfe05b53b4613532e87cd1cacc93785adc33
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-12 13:56:42 +1200

    Removed tests folder

--- __tests__/LoginScreen.test.js
+++
@@ -1,7 +0,0 @@
-import LoginScreen from "../components/screens/loginscreen/LoginScreen";
-import React from "react";
-import { render } from "react-test-renderer";
-
-it('works', () => {
-    const page = render.create(<LoginScreen/>);
-});
