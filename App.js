6b36386915d529c978fc28165855fb7d022df115
Kealen-Pillay <kealenpillay@gmail.com>
2022-04-24 15:06:00 +1200

    Removed temp home

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native';
-import RegisterScreen from './components/screens/registerscreen/RegisterScreen';
-import TempHome from './components/screens/registerscreen/TempHome';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from "react-native";
+import RegisterScreen from "./components/screens/registerscreen/RegisterScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen options={{ headerShown: false }} name="Register" component={RegisterScreen} />
-                <Stack.Screen options={{ headerShown: false }} name="Home" component={TempHome} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Register"
+          component={RegisterScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-    // container: {
-    //   flex: 1,
-    //   backgroundColor: '#fff',
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
-    // },
-});
+const styles = StyleSheet.create({});
