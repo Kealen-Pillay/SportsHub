58c537689f3eaf35084bd5217ab9db2a319f342b
kyleCS50 <kyledanielfrancis@gmail.com>
2022-04-25 15:43:10 -0700

    Tested dark mode toggle

--- components/screens/profilescreen/ProfileScreen.js
+++ components/screens/profilescreen/ProfileScreen.js
@@ -18,7 +18,7 @@ import NavGradient from "../../NavGradient";
 export var darkMode = true;
 
 const ProfileScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(true);
+  const [isEnabled, setIsEnabled] = useState(false);
 
   useEffect(() => {
     if (isEnabled) {
@@ -27,7 +27,7 @@ const ProfileScreen = () => {
       darkMode = false;
     }
   }, [isEnabled]);
-
+  
   const ratingCompleted = (rating) => {
     console.log("Rating is: " + rating);
   };
@@ -75,7 +75,7 @@ const ProfileScreen = () => {
       <View style={styles.darkModeCardContainer}>
         <Card style={styles.darkModeCard}>
           <View style={styles.darkModeInnerContainer}>
-            <Text style={styles.darkModeText}>Dark Mode:</Text>
+            <Text style={styles.darkModeText}>Light Mode:</Text>
             <Switch
               value={isEnabled}
               onValueChange={() => setIsEnabled(!isEnabled)}
