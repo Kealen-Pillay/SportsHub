01efe3f0087468b9b01f9a3cc149d77e8629f1f7
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-14 12:43:11 +1200

    styled chat screen

--- components/screens/chatscreen/ChatScreen.js
+++ components/screens/chatscreen/ChatScreen.js
@@ -15,6 +15,16 @@ const ChatScreen = ({ darkModeEnabled }) => {
         },
       ]}
     >
+      <View style={styles.titleContainer}>
+        <Text
+          style={[
+            styles.title,
+            { color: darkModeEnabled ? darkTheme.text : lightTheme.text },
+          ]}
+        >
+          CHATROOM
+        </Text>
+      </View>
       <NavGradient />
     </SafeAreaView>
   );
@@ -25,7 +35,17 @@ export default ChatScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  titleContainer: {
+    alignItems: "center",
+    width: "95%",
+    borderWidth: 2,
+    borderColor: darkTheme.pink
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 50,
   },
 });
