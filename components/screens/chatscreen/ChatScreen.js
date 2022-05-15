1440eddf27b8ef4235b979a74869f80d480f7e90
Kealen-Pillay <kealenpillay@gmail.com>
2022-05-15 15:54:32 +1200

    Reformatted styles

--- components/screens/chatscreen/ChatScreen.js
+++ components/screens/chatscreen/ChatScreen.js
@@ -14,10 +14,9 @@ import { auth } from "../../../firebase/firebase";
 import { v4 as uuid } from "uuid";
 import { firestore } from "../../../firebase/firestore";
 
-
 /**
  * TO DO:
- * 
+ *
  * -make team id's human readable so easier to reference
  * -add nested collection called "chat" or similar in a team document when it is first created
  * -finish message add to db in onSend()
@@ -25,10 +24,8 @@ import { firestore } from "../../../firebase/firestore";
  * -Link chatroom page to team page
  */
 
-
 const ChatScreen = ({ darkModeEnabled }) => {
   const [messages, setMessages] = useState([]);
-  
   const [senderName, setSenderName] = useState(null);
   const [senderAvatar, setSenderAvatar] = useState(null);
 
@@ -49,7 +46,6 @@ const ChatScreen = ({ darkModeEnabled }) => {
     getSenderInfo();
   }, []);
 
-
   const getSenderInfo = () => {
     firestore
       .collection("users")
@@ -65,27 +61,28 @@ const ChatScreen = ({ darkModeEnabled }) => {
             setSenderAvatar(senderAvatar);
           }
         });
-      });
+      })
+      .catch((error) => console.log(error));
   };
 
   const onSend = useCallback((messages = []) => {
+    
     setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages));
-      const {
+      GiftedChat.append(previousMessages, messages)
+    );
+
+    const { _id, createdAt, text, user } = messages[0];
+
+    firestore
+      .collection("teams")
+      .doc("69d84fb1-2a51-4d5c-ad46-187d6f8f3784")
+      .collection("chats") // navigate to nested chat collection and add message object to document
+      .add({
         _id,
         createdAt,
         text,
         user,
-      }=messages[0]
-      firestore.collection("teams")
-      .doc("69d84fb1-2a51-4d5c-ad46-187d6f8f3784")
-      .collection("chats")// navigate to nested chat collection and add message object to document
-    .add({
-      _id,
-      createdAt,
-      text,
-      user,
-    })
+      });
   }, []);
 
   const customtInputToolbar = (props) => {
@@ -121,22 +118,19 @@ const ChatScreen = ({ darkModeEnabled }) => {
             { color: darkModeEnabled ? darkTheme.text : lightTheme.text },
           ]}
         >
-          CHATROOM
+          Public Chat
         </Text>
       </View>
 
       <View style={styles.chatContainer}>
-      
         <GiftedChat
           messages={messages}
           onSend={(messages) => onSend(messages)}
           renderInputToolbar={(props) => customtInputToolbar(props)}
-          
-        user={{ 
-            
+          user={{
             // _id: uuid(), uncommenting this makes all bubbles same color
-            user: {senderName},
-            avatar: {senderAvatar},
+            user: { senderName },
+            avatar: { senderAvatar },
           }}
         />
 
@@ -153,22 +147,23 @@ export default ChatScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
   },
   titleContainer: {
     alignItems: "center",
     width: "95%",
     borderWidth: 2,
     borderColor: darkTheme.pink,
-    alignSelf: "center",
     justifyContent: "flex-start",
   },
   title: {
     fontWeight: "bold",
     fontSize: 50,
   },
-
   chatContainer: {
-    flex: 1,
-    marginBottom: 100,
+    marginTop: "5%",
+    height: "80%",
+    justifyContent: "flex-end",
+    width: "95%",
   },
 });
